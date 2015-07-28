167cd9ac12945cb15347bf2083901445f9647df3
Collin Melton <cmelton@stanford.edu>
2015-07-27 19:16:44 -0700

    added defaults to options

--- Worker/Startup.py
+++ Worker/Startup.py
@@ -10,14 +10,14 @@ import InstanceEngine
 # this functions gets the command line options for running the program
 def getOptions():
     parser = OptionParser()
-    parser.add_option("--S", dest = "ScriptFile", help = "this should be a text form of a startup script",
+    parser.add_option("--S", dest = "ScriptFile", help = "this should be a text form of a startup script, commands to run separated by '/n'",
                       metavar = "STRING", type = "string", default = "echo hello")
     parser.add_option("--H", dest = "HistoryFile", help = "sets location of commands history file",
-                      metavar = "STRING", type = "string", default = "/home/cmelton/StartupCommandHistory.pickle")
+                      metavar = "STRING", type = "string", default = "./StartupCommandHistory.pickle")
     parser.add_option("--N", dest = "name", help = "sets name of instance",
                       metavar = "STRING", type = "string")
-    parser.add_option("--SD", dest = "onShutdown", help = "to run on failure",
-                      metavar = "STRING", type = "string")
+    parser.add_option("--SD", dest = "onShutdown", help = "commands to run on failure separated by '/n'",
+                      metavar = "STRING", type = "string", default="echo goodbye")
     parser.add_option("--PF", dest = "performanceDataFile", help = "file to write performance data",
                       metavar = "STRING", type = "string", default = "./perfData.tsv")
     (options, args) = parser.parse_args()
