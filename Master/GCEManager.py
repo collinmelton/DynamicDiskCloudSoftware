52730a0b5c98738c9a7d7c0db4ddc6096f033b16
Collin Melton <cmelton@stanford.edu>
2015-10-31 17:16:46 -0700

    debug

--- Master/GCEManager.py
+++ Master/GCEManager.py
@@ -118,7 +118,7 @@ class GCEManager(GCENodeDriver):
         return list_volumes
 
     def localSSDData(self, location, numDisks=1):
-        return [{}]
+        return []
         return [{#'kind': 'compute#attachedDisk',
                 'autodelete': True,
                 'type': 'SCRATCH', #'local-ssd',
